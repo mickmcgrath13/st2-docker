9bd5c23de55df1ed897cbe38b8353731109d70f2
Mick McGrath <mick.mc.13@gmail.com>
2018-06-11 00:19:50 -0700

    tweaks

--- runtime/entrypoint.d/10-add-packs-bitovi-stackstorm-exchange.sh
+++ runtime/entrypoint.d/10-add-packs-bitovi-stackstorm-exchange.sh
@@ -7,4 +7,11 @@ CURRENT_BASE_PATHS="$(crudini --get /etc/st2/st2.conf content packs_base_paths)"
 EXCHANGE_PATH_STRING=",${EXCHANGE_PATH}"
 REPLACE_VAL=""
 NEW_CURRENT_BASE="${CURRENT_BASE_PATHS//,\/opt\/stackstorm\/bitovi-stackstorm-exchange/$REPLACE_VAL}"
-crudini --set /etc/st2/st2.conf content packs_base_paths "${NEW_CURRENT_BASE}""${EXCHANGE_PATH_STRING}"
+crudini --set /etc/st2/st2.conf content packs_base_paths ""
+crudini --set /etc/st2/st2.conf content packs_base_paths "${EXCHANGE_PATH}"
+
+# TODO: figure out how to specify multiple pack base paths
+#   This will cause an error when loading the sensor:
+#     ValueError: Directory "/opt/stackstorm/packs.dev,/opt/stackstorm/bitovi-stackstorm-exchange" doesn't exist
+#
+# crudini --set /etc/st2/st2.conf content packs_base_paths "${NEW_CURRENT_BASE}""${EXCHANGE_PATH_STRING}"
---
+++ runtime/st2.d/20-start-chatops.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+service st2chatops start
