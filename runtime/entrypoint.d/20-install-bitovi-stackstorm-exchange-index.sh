e344ab8a90c038deed4f4533c53bc391dd0828f3
Mick McGrath <mick.mc.13@gmail.com>
2018-06-11 21:15:31 -0700

    tweaks and cleanup

--- runtime/entrypoint.d/20-install-bitovi-stackstorm-exchange-index.sh
+++ runtime/entrypoint.d/20-install-bitovi-stackstorm-exchange-index.sh
@@ -3,5 +3,5 @@
 CURRENT_INDEX="$(crudini --get /etc/st2/st2.conf content index_url)"
 # crudini --set /etc/st2/st2.conf content index_url "${CURRENT_INDEX}",https://github.com/bitovi-stackstorm-exchange/index
 # crudini --set /etc/st2/st2.conf content index_url "${CURRENT_INDEX}",https://github.com/bitovi-stackstorm-exchange/index
-crudini --del /etc/st2/st2.conf content index_url
+# crudini --del /etc/st2/st2.conf content index_url
 crudini --set /etc/st2/st2.conf content index_url https://index.stackstorm.org/v1/index.json
