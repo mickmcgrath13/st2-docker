9d8c58f2552949add3545195f9177fe8fc1f45e2
Mick McGrath <mick.mc.13@gmail.com>
2018-06-11 23:24:50 -0700

    remove chatops volume creation from init script

--- bin/init.sh
+++ bin/init.sh
@@ -2,7 +2,6 @@
 
 # Create directories needed for bitovi stackstorm exchange development
 EXCHANGE_DIR="bitovi-stackstorm-exchange"
-CHATOPS_DIR="stackstorm-chatops"
 CONFIG_DIR="stackstorm-config"
 VIRTUALENV_DIR="stackstorm-virtualenvs"
 
@@ -10,10 +9,6 @@ VIRTUALENV_DIR="stackstorm-virtualenvs"
 
 if [ ! -d ${EXCHANGE_DIR} ]; then
   mkdir -p ${EXCHANGE_DIR}
-fi
-
-if [ ! -d ${CHATOPS_DIR} ]; then
-  mkdir -p ${CHATOPS_DIR}
 fi
 
 if [ ! -d ${CONFIG_DIR} ]; then
